6859bb507dfb0063f8070aed2f33fef713a7225a
Jon <ghostmij@gmail.com>
2020-04-05 14:37:42 -0400

    Groundwork for video browser functionality

---
+++ libraryRoot.js
@@ -0,0 +1,9 @@
+//This method will simply return the root directory for the video library
+//The root directory will vary depending on where the application is deployed
+
+function rootDir()
+{
+	//var rootDirectory = "/home/jon/mount/VideoDrive/Videos";
+	var rootDirectory = "/mnt/d/Development/GhostSync/Videos";
+	return rootDirectory;
+}
