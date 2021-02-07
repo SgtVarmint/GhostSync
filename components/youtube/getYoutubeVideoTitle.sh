ea5e32746cfcf14ae647cb9b1253a1174b30f26d
Jon <jbellotti17@gmail.com>
2021-02-06 21:26:40 -0500

    Updates youtube title shell script to not rely on needing to write to a file and adjusts PHP script to point to it

--- components/youtube/getYoutubeVideoTitle.sh
+++ components/youtube/getYoutubeVideoTitle.sh
@@ -1,9 +1,8 @@
 #NEW CODE (Updated 2/5/2021)
-wget -qO temp1 $1
-cat temp1 | grep -o '"videoPrimaryInfoRenderer":{"title":{"runs":\[{"text":.*}\]},"viewCount"' > temp2
-cat temp2 | sed s/.*:.// > temp3
-cat temp3 | sed s/\".*//
-rm temp1 temp2 temp3
+temp1=`curl $1`
+temp2=`echo $temp1 | grep -o '"videoPrimaryInfoRenderer":{"title":{"runs":\[{"text":.*}\]},"viewCount"'`
+temp3=`echo $temp2 | sed s/.*:.//`
+echo $temp3 | sed s/\".*//
 
 #OLD CODE (No longer works Feb. 2021)
 #wget -qO temp1 $1
