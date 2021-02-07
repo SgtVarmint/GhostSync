9f8af92b869bd1cdbc20971d649dc648d1f72194
Jon <jbellotti17@gmail.com>
2021-02-06 20:45:20 -0500

    Updates the youtube title bash script

--- components/youtube/getYoutubeVideoTitle.sh
+++ components/youtube/getYoutubeVideoTitle.sh
@@ -1,5 +1,13 @@
+#NEW CODE (Updated 2/5/2021)
 wget -qO temp1 $1
-cat temp1 | grep -o '\\"title\\":\\".*\\",\\"lengthSeconds\\":' > temp2
-cat temp2 | sed s/.*:..// > temp3
-cat temp3 | sed s/\\\\\".*//
+cat temp1 | grep -o '"videoPrimaryInfoRenderer":{"title":{"runs":\[{"text":.*}\]},"viewCount"' > temp2
+cat temp2 | sed s/.*:.// > temp3
+cat temp3 | sed s/\".*//
 rm temp1 temp2 temp3
+
+#OLD CODE (No longer works Feb. 2021)
+#wget -qO temp1 $1
+#cat temp1 | grep -o '\\"title\\":\\".*\\",\\"lengthSeconds\\":' > temp2
+#cat temp2 | sed s/.*:..// > temp3
+#cat temp3 | sed s/\\\\\".*//
+#rm temp1 temp2 temp3
